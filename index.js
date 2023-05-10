5997cefc90168351ab54af16ee01e36f706fac48
benjovitz <dbenjovitz@gmail.com>
2023-05-10 11:35:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//import "https://unpkg.com/navigo" //Will create the global Navigo object used below
+import "./navigo_EditedByLars.js" //Will create the global Navigo, with a few changes, object used below
+//import "./navigo.min.js" //Will create the global Navigo object used below
+
+import {
+setActiveLink, adjustForMissingHash, renderTemplate, loadTemplate
+} from "./utils.js"
+
+import {initEvents} from "./pages/events/events.js"
+
+
+window.addEventListener("load", async () => {
+
+const templateEvents = await loadTemplate("./pages/events/events.html")
+
+adjustForMissingHash()
+
+const router = new Navigo("/", { hash: true });
+//Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
+window.router = router
+
+router
+.hooks({
+before(done, match) {
+setActiveLink("menu", match.url)
+done()
+}
+})
+.on({
+"/events": () => {
+renderTemplate(templateEvents, "content")
+initEvents()
+}
+})
+.notFound(() => {
+})
+.resolve()
+});
+
+
+window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
++ ' Column: ' + column + ' StackTrace: ' + errorObj);
+}
---
+++ pages/events/events.js
@@ -0,0 +1,6 @@
+import { handleHttpErrors, sanitizeStringWithTableRows } from "../../utils.js";
+import { API_URL } from "../../settings.js";
+
+let URL = API_URL+"/events/"
+
+export async function initEvents(){}
